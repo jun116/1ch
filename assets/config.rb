03f8ae3283162d6a0f4d8eab56f7e94ceb6015c0
warppy <warppy@gmail.com>
2012-10-11 11:49:11 +0900

    add app.sass

--- assets/config.rb
+++ assets/config.rb
@@ -1,21 +1,22 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
+http_path = "..//public"
+css_dir = "../public/stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+# images_dir = "images"
+# javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style =:expanded 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
+line_comments = true
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
