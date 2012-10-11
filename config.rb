87ffd5448d2bedb3d4553ffacbb2e4eb33a4efea
warppy <warppy@gmail.com>
2012-10-12 00:17:16 +0900

    add bootstrap style.

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "./public"
-css_dir = "./public/stylesheets"
-sass_dir = "./sass"
+http_path = "public"
+css_dir = "public/stylesheets"
+sass_dir = "assets/sass"
 # images_dir = "images"
 # javascripts_dir = "javascripts"
 
